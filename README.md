bc40d5b7fed39bc2e55a2372938fd3758528d04c
jessicadoamaral07 <93171823+jessicadoamaral07@users.noreply.github.com>
2021-12-20 19:54:51 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# projeto-num
+# C12-Trex-Etapa-2
---
+++ sketch.js
@@ -0,0 +1,70 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+var rand;
+
+
+
+
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex2.png","trex3.png");
+  trex_collided = loadImage("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png");
+  
+ 
+  
+}
+
+function setup() {
+
+  createCanvas(600,200)
+  
+  //crie um sprite de trex
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.scale = 0.5;
+  
+  //crie sprite ground (solo)
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -4;
+  
+  //crie um solo invisível
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+ 
+  
+}
+
+function draw() {
+  //definir cor do plano de fundo
+  background(220);
+  
+  rand= Math.round(random(10,60));
+  console.log(rand);
+  
+  
+  // pulando o trex ao pressionar a tecla de espaço
+  if(keyDown("space")&& trex.y >= 100) {
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  //impedir que o trex caia
+  trex.collide(invisibleGround);
+
+  
+  drawSprites();
+  
+}
+
+
+
